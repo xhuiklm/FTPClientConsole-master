6d54fde784432906ceb29dee61ec727e2cd122d7
xhuiklm <dsh2t97@gmail.com>
2017-12-12 21:37:25 +0700

    cerk

--- FTPClient/HaiDang.h
+++ FTPClient/HaiDang.h
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include "winsock2.h"
-class HaiDang
-{
-public:
+
 	int upload(SOCKET ftpControl, SOCKET dataControl);
 	int show(SOCKET ftpControl, SOCKET dataControl);
 	int Quit(SOCKET ftpControl, SOCKET dataControl);
 	int download(SOCKET ftpControl, SOCKET dataControl);
-};
 
